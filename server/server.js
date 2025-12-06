7a23d66a1ab2ac9df42f644a85886aa48b8f2312
Ritesh Kumar <riteshrajudrk@gmail.com>
2025-12-06 23:22:01 +0530

    vercel.json added

--- server/server.js
+++ server/server.js
@@ -44,7 +44,11 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+}
+
+export default app;
